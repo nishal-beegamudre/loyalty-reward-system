eb9d86ae56b15942c2f839752ba1c6e8162beb6b
Nishal Beegamudre <nishalb1999@gmail.com>
2024-11-20 17:36:18 +0530

    Delete adminservice/adminservice directory

--- adminservice/adminservice/src/main/java/com/adminservice/util/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.adminservice.util;
-
-public class Constants {
-	
-	public static final String IST = "Asia/Kolkata";
-	public static final String INCORRECT_MASTER_KEY = "INCORRECT MASTER KEY";
-	public static final String EXCEPTION_OCCURRED = "EXCEPTION OCCURRED";
-
-}
